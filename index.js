67ca7701ba0a17f7dc5583b26b1d61d57e0f3561
Adam <emailnasa@gmail.com>
2022-02-13 14:48:45 -0500

    Created javascript mouseover events for links to display labels as to where the links go.

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 const blogModal = document.querySelector(".latestpost")
 const blogbtn = document.querySelector("#blog");
 const projectSelect = document.querySelector("#projects");
-const closeBlogModal = document.querySelector("#closeblog")
+const closeBlogModal = document.querySelector("#closeblog");
+const linkHovers = document.querySelectorAll(".link-hover");
+const underLinkModals = document.querySelectorAll(".underlink");
 
 const blogButton = function () {
     if (!blogModal.classList.contains("hidden")) {
-        blogModal.classList.add("hidden")   
+        blogModal.classList.add("hidden");   
     } else {
-        blogModal.classList.remove("hidden")   
+        blogModal.classList.remove("hidden");   
     }
 }
 
-blogbtn.addEventListener("click", blogButton)
+blogbtn.addEventListener("click", blogButton);
+
 closeBlogModal.addEventListener("click", function () {
-    blogModal.classList.add("hidden")
-})
+    blogModal.classList.add("hidden");
+});
+
+// linkHovers[1].addEventListener("mouseover", function () {
+//          underLinkModals[1].classList.remove("hidden");
+// });
+
+
+for (let i = 0; i < linkHovers.length; i++) {
+    linkHovers[i].addEventListener("mouseover", function () {
+        underLinkModals[i].classList.remove("hidden");
+    });
+        linkHovers[i].addEventListener("mouseout", function () {
+            underLinkModals[i].classList.add("hidden");
+    });
+}
